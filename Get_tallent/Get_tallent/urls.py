5bebb781865dee1c8ce78faeed1e8524b411ee3a
Supremepraiz <supremepraiz2@gmail.com>
2024-04-02 12:36:54 +0100

    Last Updated project
    vend tallent

--- Get_tallent/Get_tallent/urls.py
+++ Get_tallent/Get_tallent/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("tallents/", include("tallent_app.api.urls"))
+    path("tallent/", include("tallent_app.api.urls"))
 ]
--- Get_tallent/tallent_app/models.py
+++ Get_tallent/tallent_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Employer(models.Model):
+    pass
+
+class Worker(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
