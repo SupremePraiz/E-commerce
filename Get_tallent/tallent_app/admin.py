0ce47247632551b6b20b5f2dbc37756aa1684e81
Supremepraiz <supremepraiz2@gmail.com>
2024-05-02 00:16:28 +0100

    made changes to models.py

--- Get_tallent/tallent_app/admin.py
+++ Get_tallent/tallent_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Contractor, Employer, JobPost, Comment
+
 # Register your models here.
+
+
+
+admin.site.register(Contractor)
+admin.site.register(Employer)
+admin.site.register(JobPost)
+admin.site.register(Comment)
--- Get_tallent/tallent_app/models.py
+++ Get_tallent/tallent_app/models.py
@@ -1,15 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
 
-# Create your models here.
+class Contractor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='contractor_images/')
+    about = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=15, validators=[RegexValidator(r'^\+?1?\d{9,15}$')])  # Example regex for phone number validation
+    date = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
 class Employer(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, upload_to='employer_images/')
+    about = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=15, validators=[RegexValidator(r'^\+?1?\d{9,15}$')])  # Example regex for phone number validation
+    date = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
 
-class Worker(models.Model):
-    pass
+
+class JobPost(models.Model):
+    post = models.TextField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+   
 
 class Comment(models.Model):
-    pass
-
-
-class Post(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    jobpost = models.ForeignKey(JobPost, on_delete=models.CASCADE, related_name='comments')
+    review = models.TextField(max_length=500)  # Adjust max_length as needed
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
